32145a44e3bbee038e3b30fd1c45936ea8ce318d
Magesh Kuppan <tkmagesh77@gmail.com>
2024-05-28 14:31:38 +0530

    assignment solution

--- 04-assignments/02-assignment.go
+++ 04-assignments/02-assignment.go
@@ -1,3 +1,21 @@
 /*
 Accept a range from the user and print all the prime numbers between the given range
 */
+package main
+
+import "fmt"
+
+func main() {
+	var start, end int
+	fmt.Println("Enter the range")
+	fmt.Scanln(&start, &end)
+LOOP:
+	for no := start; no <= end; no++ {
+		for i := 2; i <= (no / 2); i++ {
+			if no%i == 0 {
+				continue LOOP
+			}
+		}
+		fmt.Println("Prime No :", no)
+	}
+}
