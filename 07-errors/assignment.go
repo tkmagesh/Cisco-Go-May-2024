e0a6727abad0d75b37b8d857dcb7668501d64252
Magesh Kuppan <tkmagesh77@gmail.com>
2024-05-29 09:44:30 +0530

    updates

--- 07-errors/assignment.go
+++ 07-errors/assignment.go
@@ -1,10 +1,35 @@
 package main
 
-func main(){
-	// divide with y == 0
-	// divide with y != 0
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	divisor := 7
+	if q, r, err := divide(100, divisor); err == nil {
+		fmt.Printf("Dividing 100 by %d, quotient = %d and remainder = %d\n", divisor, q, r)
+	} else {
+		fmt.Println("error :", err)
+	}
 }
 
-func divide(x, y int) /* ???? */ {
-	return error if y == 0 OR quotent & remainder
+/*
+func divide(x, y int) (int, int, error) {
+	if y == 0 {
+		return 0, 0, errors.New("divisor cannot be zero")
+	}
+	quotient, remainder := x/y, x%y
+	return quotient, remainder, nil
+}
+*/
+
+// using named result
+func divide(x, y int) (quotient, remainder int, err error) {
+	if y == 0 {
+		err = errors.New("divisor cannot be zero")
+		return
+	}
+	quotient, remainder = x/y, x%y
+	return
 }
--- 07-errors/program.go
+++ 07-errors/program.go
@@ -6,6 +6,8 @@ import (
 	"math/rand"
 )
 
+var ErrDivideByZero error = errors.New("divisor cannot be zero")
+
 func main() {
 	/*
 		for range 20 {
@@ -18,13 +20,24 @@ func main() {
 		}
 	*/
 
-	for range 20 {
-		evenNo, err := getRandEvenNo()
-		if err == nil {
-			fmt.Println("Even number generated successfully :", evenNo)
-			continue
+	/*
+		for range 20 {
+			evenNo, err := getRandEvenNo()
+			if err == nil {
+				fmt.Println("Even number generated successfully :", evenNo)
+				continue
+			}
+			fmt.Println("error :", err)
 		}
-		fmt.Println("error :", err)
+	*/
+
+	divisor := 0
+	if q, r, err := divide(100, divisor); err == nil {
+		fmt.Printf("Dividing 100 by %d, quotient = %d and remainder = %d\n", divisor, q, r)
+	} else if err == ErrDivideByZero {
+		fmt.Println("do not attempt to divide by 0")
+	} else {
+		fmt.Println("unknown error :", err)
 	}
 
 }
@@ -47,4 +60,13 @@ func getRandEvenNo() (int, error) {
 	var err error
 	err = errors.New("Unable to generate random even number")
 	return 0, err
+}
+
+func divide(x, y int) (quotient, remainder int, err error) {
+	if y == 0 {
+		err = ErrDivideByZero
+		return
+	}
+	quotient, remainder = x/y, x%y
+	return
 }
