bbe671fe8f024559a64fc8b0430485f0f17831fe
Magesh Kuppan <tkmagesh77@gmail.com>
2024-05-29 10:38:15 +0530

    updates

--- 09-pointers/assignment.go
+++ 09-pointers/assignment.go
@@ -5,11 +5,11 @@ import "fmt"
 func main() {
 	n1, n2 := 100, 200
 	fmt.Printf("Before swapping, n1 = %d and n2 = %d\n", n1, n2)
-	swap( /* ???? */ )
+	swap(&n1, &n2)
 	fmt.Printf("After swapping, n1 = %d and n2 = %d\n", n1, n2)
 
 }
 
-func swap( /* ?? */ ) /* no return results */ {
-	/* ?? */
+func swap(x, y *int) /* no return results */ {
+	*x, *y = *y, *x
 }
