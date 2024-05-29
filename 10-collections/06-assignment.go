dcaa8bd1b296b9fd5421a36dc6eeb899afb33405
Magesh Kuppan <tkmagesh77@gmail.com>
2024-05-29 13:33:28 +0530

    assignment

---
+++ 10-collections/06-assignment.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	str := "Incididunt laborum velit minim ex excepteur nostrud reprehenderit occaecat occaecat ea occaecat dolore in irure Non nulla incididunt occaecat enim eiusmod irure dolor Velit ut voluptate adipisicing elit officia aute aute et magna magna deserunt dolor Exercitation ipsum consequat tempor reprehenderit Laborum Lorem fugiat sit voluptate eiusmod Aliqua pariatur do ad commodo deserunt cupidatat incididunt sint ad in do incididuntAliquip laboris mollit qui ex sint esse Eu adipisicing laborum in mollit non commodo deserunt consectetur Amet excepteur commodo incididunt nostrud commodo ipsum Velit et enim sunt anim ea excepteur cupidatat Velit Lorem incididunt aute mollit id esse cillum reprehenderit occaecat officia adipisicing Occaecat irure duis sint laboris velit Irure eu nostrud esse sit excepteur occaecat ea culpa esse tempor quis et mollit deseruntNostrud minim incididunt sunt dolore sit irure fugiat irure Nisi commodo anim non culpa Ut consectetur nisi esse est Lorem sunt est laboris sunt laborum"
+
+	/* find the "word size that occurs the most" and print the word size and the count
+	ex:
+		4 letter words occurs the most with 30 occurrances
+	*/
+}
