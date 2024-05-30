e6f5ba9253c27a1dc6191ce14461337c1c086589
Magesh Kuppan <tkmagesh77@gmail.com>
2024-05-30 14:50:56 +0530

    assignment solution

--- 16-concurrency/04-communication/15-assignment.go
+++ 16-concurrency/04-communication/15-assignment.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -12,30 +12,86 @@ import (
 // consumer
 func main() {
 	start := time.Now()
-	fmt.Println(add(100, 200))
-	fmt.Println(subtract(100, 200))
-	fmt.Println(multiply(100, 200))
-	fmt.Println(divide(100, 200))
+	addCh := func() chan int {
+		ch := make(chan int)
+		go func() {
+			result := add(100, 200)
+			ch <- result
+		}()
+		return ch
+	}()
+	subtractCh := func() chan int {
+		ch := make(chan int)
+		go func() {
+			result := subtract(100, 200)
+			ch <- result
+		}()
+		return ch
+	}()
+	multiplyCh := func() chan int {
+		ch := make(chan int)
+		go func() {
+			result := multiply(100, 200)
+			ch <- result
+		}()
+		return ch
+	}()
+	divideCh := func() chan int {
+		ch := make(chan int)
+		go func() {
+			result := divide(100, 200)
+			ch <- result
+		}()
+		return ch
+	}()
+
+	wg := sync.WaitGroup{}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println("Add Result :", <-addCh)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println("Subtract Result :", <-subtractCh)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println("Multiply Result :", <-multiplyCh)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println("Divide Result :", <-divideCh)
+	}()
+
+	wg.Wait()
 	elapsed := time.Since(start)
 	fmt.Println("Time taken :", elapsed)
 }
 
 func add(x, y int) int {
-	time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
+	time.Sleep(time.Duration(5) * time.Second)
 	return x + y
 }
 
 func subtract(x, y int) int {
-	time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
+	time.Sleep(time.Duration(5) * time.Second)
 	return x - y
 }
 
 func multiply(x, y int) int {
-	time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
+	time.Sleep(time.Duration(5) * time.Second)
 	return x * y
 }
 
 func divide(x, y int) int {
-	time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
+	time.Sleep(time.Duration(5) * time.Second)
 	return x / y
 }
